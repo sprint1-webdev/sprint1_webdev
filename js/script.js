2bba1617631a7b95076b033bbd03babcfdb0a20b
Felipe <felipenafe@gmail.com>
2024-05-27 18:26:43 -0300

    Henrique Ignacio Bartalo / rm555274

--- js/script.js
+++ js/script.js
@@ -79,3 +79,15 @@ form.addEventListener("submit", (event) => {
   // se todos os capos estiverem preenchidos enviar form
   form.submit();
 });
+
+// VALIDANDO O USUÁRIO NA PÁGINA DE LOGIN
+function validar(){
+    //declarando a variavel
+    let usuario =document.getElementById("usuario").value;
+    let senha =document.getElementById("senha").value;
+    if(usuario == "Admin" && senha === "12345"){
+        window.open('quiz.html');
+    }else{
+        alert("usuario e senha inválidos");
+    }
+}
