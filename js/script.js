2d7bd2ee7482ce9e36253fb861618da8f2b70ecd
Felipe <felipenafe@gmail.com>
2024-05-27 18:16:54 -0300

    pagina contato finalizada / João VIctor Soave rm557595

--- js/script.js
+++ js/script.js
@@ -34,3 +34,48 @@ function slideShow() {
     }
     setTimeout('slideShow()', time);
 }
+
+
+// REDIRECIONANDO OS BOTÕES
+function redirect1() {
+    window.location.href = 'https://www.mahindraracing.com/drivers/nyck-de-vries';
+}
+
+function redirect2() {
+    window.location.href = 'https://www.mahindraracing.com/drivers/edoardo-mortara';
+}
+
+
+
+// VALIDANDO OS CAMPOS DO CONTATO
+
+// Declarando as variáveis
+const form = document.querySelector("#form")
+const nameInput = document.querySelector("#name")
+const emailInput = document.querySelector("#email")
+const telInput = document.querySelector("#telefone")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    //verifica se o nome esta vazio
+    if(nameInput.value === ""){
+        alert("Preencha seu nome")
+        return;
+    }
+
+    //verifica se o email esta vazio
+    if(emailInput.value === ""){
+        alert("Preencha seu seu email")
+        return;
+    }
+
+    //verifica se o telefone esta vazio
+    if(telInput.value === ""){
+        alert("Preencha seu seu telefone")
+        return;
+    }
+
+  // se todos os capos estiverem preenchidos enviar form
+  form.submit();
+});
