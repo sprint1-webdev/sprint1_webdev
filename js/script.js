cbdb93c3d71cff10fbf19c10def19fe846a4d8ae
Felipe <felipenafe@gmail.com>
2024-05-28 01:02:46 -0300

    Felipe Fernandes rm554598 projeto finalizado

--- js/script.js
+++ js/script.js
@@ -86,7 +86,7 @@ function validar(){
     let usuario =document.getElementById("usuario").value;
     let senha =document.getElementById("senha").value;
     if(usuario == "Admin" && senha === "12345"){
-        window.open('quiz.html');
+        window.open('sobre.html');
     }else{
         alert("usuario e senha inválidos");
     }
